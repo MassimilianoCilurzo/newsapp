7126e3459326f82d611745d4f760c65706286e02
MassimilianoCilurzo <massimiliano.cilurzo@gmail.com>
2021-07-28 14:54:42 +0200

    Add files via upload

---
+++ app/src/main/java/android/example/newsguardianapp/NewsActivity.java
@@ -0,0 +1,245 @@
+package android.example.newsguardianapp;
+
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<List<News>> {
+
+    private static final String LOG_TAG = NewsActivity.class.getName();
+
+    private static final String QUERY_PARAM = "q";
+    private static final String ORDER_BY_PARAM = "order-by";
+    private static final String PAGE_SIZE_PARAM = "page-size";
+    private static final String ORDER_DATE_PARAM = "order-date";
+    private static final String FROM_DATE_PARAM = "from-date";
+    private static final String SHOW_FIELDS_PARAM = "show-fields";
+    private static final String FORMAT_PARAM = "format";
+    private static final String SHOW_TAGS_PARAM = "show-tags";
+    private static final String API_KEY_PARAM = "api-key";
+    private static final String SECTION_PARAM = "section";
+    public static final String SHOW_FIELDS = "trailText";
+    /**
+     * The format we want our API to return
+     */
+    public static final String FORMAT = "json";
+    /**
+     * The show tags we want our API to return
+     */
+    public static final String SHOW_TAGS = "contributor";
+    /**
+     * API Key
+     */
+    public static final String API_KEY = "test";
+    /**
+     * Default number to set the image on the top of the textView
+     */
+    public static final int DEFAULT_NUMBER = 0;
+    /**
+     * HTTP request parameters
+     */
+    static final int READ_TIMEOUT = 10000; /* milliseconds */
+    static final int CONNECT_TIMEOUT = 15000; /* milliseconds */
+    static final int SUCCESS_RESPONSE_CODE = 200;
+    static final String REQUEST_METHOD_GET = "GET";
+    /**
+     * Extract the key associated with the JSONObject
+     */
+    static final String JSON_KEY_RESPONSE = "response";
+    static final String JSON_KEY_RESULTS = "results";
+    static final String JSON_KEY_WEB_TITLE = "webTitle";
+    static final String JSON_KEY_AUTHOR = "webTitle";
+    static final String JSON_KEY_SECTION_NAME = "sectionName";
+    static final String JSON_KEY_WEB_PUBLICATION_DATE = "webPublicationDate";
+    static final String JSON_KEY_WEB_URL = "webUrl";
+    static final String JSON_KEY_TAGS = "tags";
+    static final String JSON_KEY_FIELDS = "fields";
+
+
+
+    /**
+     * URL for earthquake data from the USGS dataset
+     */
+    private static final String GUARDIAN_REQUEST_URL =
+            "https://content.guardianapis.com/search";
+
+    /**
+     * Constant value for the earthquake loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int NEWS_LOADER_ID = 1;
+
+    /**
+     * Adapter for the list of earthquakes
+     */
+    private NewsAdapter mAdapter;
+
+    /**
+     * TextView that is displayed when the list is empty
+     */
+    private TextView mEmptyStateTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_activity);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView newsListView = (ListView) findViewById(R.id.list);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
+        // Create a new adapter that takes an empty list of earthquakes as input
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        newsListView.setAdapter(mAdapter);
+
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected earthquake.
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current earthquake that was clicked on
+                News currentEarthquake = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+
+                // Create a new intent to view the earthquake URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // getString retrieves a String value from the preferences
+        // the second parameter is the default value for this preference.
+        String numbOfItems = sharedPrefs.getString(
+                getString(R.string.settings_number_of_items_key),
+                getString(R.string.settings_number_of_items_default));
+
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+
+
+
+
+
+
+        // Parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value. For example, the `format=json`
+        uriBuilder.appendQueryParameter(QUERY_PARAM, "");
+        uriBuilder.appendQueryParameter(API_KEY_PARAM,API_KEY);
+        uriBuilder.appendQueryParameter(ORDER_BY_PARAM, orderBy);
+        uriBuilder.appendQueryParameter(PAGE_SIZE_PARAM, numbOfItems);
+        uriBuilder.appendQueryParameter(FORMAT_PARAM, FORMAT);
+
+
+        // Return the completed uri
+        return new NewsLoader(this, uriBuilder.toString());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> articles) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No earthquakes found."
+        mEmptyStateTextView.setText(R.string.no_news);
+
+        // Clear the adapter of previous earthquake data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (articles != null && !articles.isEmpty()) {
+            mAdapter.addAll(articles);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
